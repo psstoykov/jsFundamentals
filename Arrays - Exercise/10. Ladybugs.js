3a25ecffc912062fa8608b15288315c256d39968
psstoykov <145485240+psstoykov@users.noreply.github.com>
2023-10-04 11:40:26 +0200

    HTML & CSS exercises

---
+++ Arrays - Exercise/10. Ladybugs.js
@@ -0,0 +1,15 @@
+function solve() {
+
+
+};
+
+solve([3, '0 1',
+    '0 right 1',
+    '2 right 1']);
+solve([3, '0 1 2',
+    '0 right 1',
+    '1 right 1',
+    '2 right 1']);
+solve([5, '3',
+    '3 left 2',
+    '1 left -2']);
