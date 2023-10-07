7c83f5ee7c0cbf29e0059d5068f29e412c5d17ed
psstoykov <145485240+psstoykov@users.noreply.github.com>
2023-10-07 13:05:49 +0200

    LadyBugs completed

--- Arrays - Exercise/10. Ladybugs.js
+++ Arrays - Exercise/10. Ladybugs.js
@@ -1,74 +1,73 @@
-function solve(array) {
+function ladyBugs(array) {
     let size = array[0]; // the lenght of the field as integer             
     let field = new Array(size); // the empty field array is created
     let positions = array[1].split(" "); //positions are formated in an array as !!strings!!
 
-    //filling field with 0s
-    for (let i = 0; i < field.length; i++) {
+    for (let i = 0; i < field.length; i++) {     //filling field with 0s
         field[i] = 0;
     }
 
-    //filling the field array with the bugs
-    for (let i = 0; i < positions.length; i++) {
+    for (let i = 0; i < positions.length; i++) {    //filling the field array with the bugs
         if (Number(positions[i] >= 0 && Number(positions[i]) < field.length) && field[Number(positions[i])] == 0) {
 
             field[Number(positions[i])]++;
         }
     }
 
-    //initialize the array for the fly commands
-    let commands = [];
+    let commands = [];  //initialize the array for the fly commands
     for (let i = 2; i < array.length; i++) {
         commands.push(array[i]);
     }
 
-    let index = 0;
-    ////forloop defining the steps for the bugs
     for (let i = 0; i < commands.length; i++) {
         let steps = commands[i].split(" ");
         let bug = Number(steps[0]);
         let direction = steps[1];
-        let distance = Number(steps[2]);
+        let flyPath = Number(steps[2]);
 
+        let newPosition = 0;
 
-        //if we have a bug on given position, move it to a new position
-        if (field[bug] == 1 && distance != 0) {
-            //if direction is right/left
-            if (direction == "right") {
-                while (field[bug + distance] != 0 && (bug + distance <= field.length) && (bug + distance >= 0)) {
-                    distance += Number(steps[2]);
-                }
-                if ((bug + distance >= 0) && (bug + distance <= field.length)) {
-                    field[bug + distance] = 1;  //place the bug at the new location
-                }
-                field[bug] = 0; //removed the bug from starting position
-            } else if (direction == "left") {
-                while (field[bug - distance] != 0 && (bug - distance >= 0) && (bug - distance <= field.length)) {
-                    distance += Number(steps[2]);
-                }
-                if ((bug - distance >= 0) && (bug - distance <= field.length)) {
-
-                    field[bug - distance] = 1;
-                }
-                field[bug] = 0;
-            }
+        if (flyPath == 0) { //if no flyPath, continue to next iteration
+            continue;
         }
-    }
+        if (field[bug] == 1) {// if we have a bug on the given command, then fly
+            if (direction == "right") {
+                newPosition = bug + flyPath;
+                while (field[newPosition] == 1) {
+                    newPosition += flyPath;
+                }
+                if (newPosition < field.length && newPosition >= 0) {
+                    field[newPosition] = 1
+                }
+            } else if (direction == "left") {
+                newPosition = bug - flyPath;
+                while (field[newPosition] == 1 && newPosition >= 0) {
+                    newPosition -= flyPath;
+                }
+                if (newPosition < field.length && newPosition >= 0) {
+                    field[newPosition] = 1
+                }
+            }
+            field[bug] = 0; //remove bug from it's initial position
+        }
+
+    };
     let result = field.join(" ")
     console.log(result)
+};
 
-}
+ladyBugs([3, '1 2',
+    '2 left 2',
+    '1 left 1']);
+ladyBugs([3, '0 1',
+    '0 right 1',
+    '2 right 1']);
 
+ladyBugs([3, '0 1 2',
+    '0 right 1',
+    '1 right 1',
+    '2 right 1']);
 
-// solve([3, '0 1',
-//     '0 right 1',
-//     '2 right 1']);
-
-// solve([3, '0 1 2',
-//     '0 right 1',
-//     '1 right 1',
-//     '2 right 1']);
-
-// solve([5, '3',
-//     '3 left 2',
-//     '1 left -2']);
+ladyBugs([5, '3',
+    '3 left 2',
+    '1 left -2']);
