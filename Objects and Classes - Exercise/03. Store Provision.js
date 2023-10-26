56bdde489038c5cb0f1865ed3a448db41e398321
psstoykov <145485240+psstoykov@users.noreply.github.com>
2023-10-26 18:48:25 +0200

    objects homework

--- Objects and Classes - Exercise/03. Store Provision.js
+++ Objects and Classes - Exercise/03. Store Provision.js
@@ -1,24 +1,45 @@
-function solve(arr1, arr2) {
+function solve(inventory, ordered) {
 
+    let stock = {};
 
+    for (let i = 0; i < inventory.length; i += 2) {
+        let product = inventory[i];
+        let quantiy = Number(inventory[i + 1]);
+        stock[`${product}`] = quantiy;
+    }
+    for (let i = 0; i < ordered.length; i += 2) {
+        let newOrder = ordered[i];
+        let quantiy = Number(ordered[i + 1]);
+        if (stock[`${newOrder}`]) {
+            stock[`${newOrder}`] += quantiy;
+        } else {
+            stock[`${newOrder}`] = quantiy;
+        }
+    }
+
+    for (let [key, value] of Object.entries(stock)) {
+        // array.push(key);
+        // array.push(value);
+        console.log(`${key} -> ${value}`);
+    }
 };
 
-solve([
-    'Chips', '5', 'CocaCola', '9', 'Bananas',
-    '14', 'Pasta', '4', 'Beer', '2'
-],
-    [
-        'Flour', '44', 'Oil', '12', 'Pasta', '7',
-        'Tomatoes', '70', 'Bananas', '30'
-    ]
-);
-
 // solve([
-//     'Salt', '2', 'Fanta', '4', 'Apple', '14',
-//     'Water', '4', 'Juice', '5'
+//     'Chips', '5', 'CocaCola', '9', 'Bananas',
+//     '14', 'Pasta', '4', 'Beer', '2'
 // ],
 //     [
-//         'Sugar', '44', 'Oil', '12', 'Apple', '7',
-//         'Tomatoes', '7', 'Bananas', '30'
+//         'Flour', '44', 'Oil', '12', 'Pasta', '7',
+//         'Tomatoes', '70', 'Bananas', '30'
 //     ]
 // );
+
+solve([
+    'Salt', '2', 'Fanta', '4', 'Apple', '14',
+    'Water', '4', 'Juice', '5'
+],
+    [
+        'Sugar', '44', 'Oil', '12', 'Apple', '7',
+        'Tomatoes', '7', 'Bananas', '30'
+    ]
+);
